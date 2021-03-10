e67515dcf3013dbd8dde44ba8df6133837c903cb
Emily Thomas <ethomas@quint.co.uk>
2021-03-09 22:25:45 -0800

    added mongoose database

---
+++ server.js
@@ -0,0 +1,10 @@
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
