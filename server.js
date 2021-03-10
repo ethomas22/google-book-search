0faa315a7738fb9714db11048a28c099870682c3
Emily Thomas <ethomas@quint.co.uk>
2021-03-09 22:48:09 -0800

    added express server

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+require("./routes/api")(app);
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+    app.listen(PORT, function () {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+    });
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
     {
